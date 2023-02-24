ee99d60485f7b495988969505677bd27d8db98ee
Lenz Lösche <lenzloesche@gmail.com>
2023-02-24 13:13:53 +0100

    add nav function plus cursor pointer

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { useState } from "react";
 
 function App() {
   const [isFavorite, setIsFavorite] = useState([false, false, false]);
+  const [allEntriesSelected, setAllEntriesSelected] = useState(true);
   const numberOfFavorites = () => {
     let trueCounter = 0;
     for (let count = 0; count < isFavorite.length; count++) {
@@ -27,6 +28,14 @@ function App() {
     setIsFavorite(newIsFavorite);
   }
 
+  function handleNavClick(number) {
+    if (number === 0) {
+      setAllEntriesSelected(true);
+    } else {
+      setAllEntriesSelected(false);
+    }
+  }
+
   return (
     <main>
       <Header>JOURNAL</Header>
@@ -37,51 +46,67 @@ function App() {
       <Navigation>
         <NavigationItem
           title="All Entries"
+          onClick={() => {
+            handleNavClick(0);
+          }}
           number={isFavorite.length}
-          selected
+          selected={allEntriesSelected ? true : false}
         />
-        <NavigationItem title="Favorites" number={numberOfFavorites()} />
+        <NavigationItem
+          title="Favorites"
+          onClick={() => {
+            handleNavClick(1);
+          }}
+          number={numberOfFavorites()}
+          selected={allEntriesSelected ? false : true}
+        />
       </Navigation>
       <EntryContainer>
-        <Entry
-          title="Lorem1"
-          date="FEB 27, 2028"
-          onStarClick={() => {
-            handleStarClick(0);
-          }}
-          bookmarked={isFavorite[0] ? true : false}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          consectetur totam unde quas. Nisi nemo, facere cumque dolores optio
-          temporibus magni placeat sed, libero nulla quae quam impedit excepturi
-          voluptas.
-        </Entry>
-        <Entry
-          title="Lorem2"
-          date="JAN 8, 2028"
-          onStarClick={() => {
-            handleStarClick(1);
-          }}
-          bookmarked={isFavorite[1] ? true : false}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          consectetur totam unde quas. Nisi nemo, facere cumque dolores optio
-          temporibus magni placeat sed, libero nulla quae quam impedit excepturi
-          voluptas.
-        </Entry>
-        <Entry
-          title="Lorem Next"
-          date="FEB 5, 2028"
-          onStarClick={() => {
-            handleStarClick(2);
-          }}
-          bookmarked={isFavorite[2] ? true : false}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          consectetur totam unde quas. Nisi nemo, facere cumque dolores optio
-          temporibus magni placeat sed, libero nulla quae quam impedit excepturi
-          voluptas.
-        </Entry>
+        {allEntriesSelected ? (
+          <>
+            <Entry
+              title="Lorem1"
+              date="FEB 27, 2028"
+              onStarClick={() => {
+                handleStarClick(0);
+              }}
+              bookmarked={isFavorite[0] ? true : false}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+              consectetur totam unde quas. Nisi nemo, facere cumque dolores
+              optio temporibus magni placeat sed, libero nulla quae quam impedit
+              excepturi voluptas.
+            </Entry>
+            <Entry
+              title="Lorem2"
+              date="JAN 8, 2028"
+              onStarClick={() => {
+                handleStarClick(1);
+              }}
+              bookmarked={isFavorite[1] ? true : false}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+              consectetur totam unde quas. Nisi nemo, facere cumque dolores
+              optio temporibus magni placeat sed, libero nulla quae quam impedit
+              excepturi voluptas.
+            </Entry>
+            <Entry
+              title="Lorem Next"
+              date="FEB 5, 2028"
+              onStarClick={() => {
+                handleStarClick(2);
+              }}
+              bookmarked={isFavorite[2] ? true : false}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+              consectetur totam unde quas. Nisi nemo, facere cumque dolores
+              optio temporibus magni placeat sed, libero nulla quae quam impedit
+              excepturi voluptas.
+            </Entry>
+          </>
+        ) : (
+          ""
+        )}
       </EntryContainer>
       <Footer>Journal App -- 2028</Footer>
     </main>
--- src/components/NavigationItem/index.js
+++ src/components/NavigationItem/index.js
@@ -1,7 +1,7 @@
 import "./NavigationItem.css";
-export default function NavigationItem({ title, number, selected }) {
+export default function NavigationItem({ title, number, selected, onClick }) {
   return (
-    <li className="listItem ">
+    <li className="listItem " onClick={onClick}>
       <p className={`text ${selected ? "text--bolded" : ""}`}>{title}</p>
       <p className={`number ${selected ? "number--bolded" : ""}`}>{number}</p>
     </li>
