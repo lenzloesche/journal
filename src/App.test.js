7978b89e5b8cb7f74e62718b233287eed2c91ba1
Lenz Lösche <lenzloesche@gmail.com>
2023-02-25 10:54:58 +0100

    add test for button klick

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { toBeInTheDocument } from "@testing-library/jest-dom/dist/matchers";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("CreateButton", () => {
@@ -7,6 +8,42 @@ test("CreateButton", () => {
   expect(createButton).toBeInTheDocument();
   const createButtoninnerHTML = createButton.innerHTML;
   expect(createButtoninnerHTML).toContain("Create");
+});
+
+test("button click should create a new entry (if text in both input fields) with the given text", () => {
+  render(<App />);
+  const createButton = screen.getByRole(/button/i);
+  const entriesOld = screen.getAllByRole("article");
+  const textboxes = screen.getAllByRole("textbox");
+  textboxes.forEach((textbox, index) => {
+    fireEvent.change(textbox, {
+      target: { value: "TestInputedtthrt" + index },
+    });
+  });
+  fireEvent.click(createButton);
+  const entriesNew = screen.getAllByRole("article");
+  expect(entriesNew.length).toBe(entriesOld.length + 1);
+  const testInputText = screen.getByText(/TestInputedtthrt0/i);
+  expect(testInputText).toBeInTheDocument();
+});
+
+test("button click should not create a new entry if one of input fields is empty", () => {
+  render(<App />);
+  const createButton = screen.getByRole(/button/i);
+  const entriesOld = screen.getAllByRole("article");
+  const textboxes = screen.getAllByRole("textbox");
+  textboxes.forEach((textbox, index) => {
+    let testText = "";
+    if (index === 0) {
+      testText = "wivbwiuezvbweuizbvweuihef";
+    }
+    fireEvent.change(textbox, {
+      target: { value: testText },
+    });
+  });
+  fireEvent.click(createButton);
+  const entriesNew = screen.getAllByRole("article");
+  expect(entriesNew.length).toBe(entriesOld.length);
 });
 
 test("inputfields", () => {
@@ -20,4 +57,5 @@ test("inputfields", () => {
     expect(textbox).toBeInTheDocument();
     expect(["Motto", "Notes"]).toContain(textbox.id);
   });
+  expect(textboxes.length).toBe(2);
 });
